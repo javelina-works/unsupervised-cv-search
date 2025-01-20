a8fa1c7383d16ea623e95cb14c2b0eaeb82faeb8
Kellan Cerveny <kjcerveny@gmail.com>
2025-01-20 11:58:39 -0600

    Able to interact with the fresh targets GDF

--- content/notebooks/jupytext/target_audit.py
+++ content/notebooks/jupytext/target_audit.py
@@ -571,7 +571,7 @@ from plant_search.vegetation_indices import normalize_rgb, calculate_exg
 vegetation_indices = True
 smoothing = True
 contrast_enhancement = False
-morphological = False
+morphological = True
 
 def full_veg_index(image):
     # Normalize ExG to the range [0, 255] for OpenCV compatibility
@@ -672,7 +672,7 @@ import geopandas as gpd
 
 from ipyleaflet.projections import projections
 
-def plot_targets_on_map(region_geojson, depots_filename, micro_routes_filename, targets_plants_filename):
+def plot_targets_on_map(region_geojson, targets_gdf):
 
     # Get image data
     # image, transform, bounds, image_crs = load_image(orthophoto_path)
@@ -681,27 +681,6 @@ def plot_targets_on_map(region_geojson, depots_filename, micro_routes_filename, 
         region_contour_data = json.load(f)
     region_geometry = shape(region_contour_data['features'][0]['geometry'])
     region_center = region_geometry.centroid
-
-    with open(depots_filename, "r") as f:
-        depot_data = json.load(f)
-
-    with open(micro_routes_filename, "r") as f:
-        micro_routes_data = json.load(f)
-
-    with open(targets_plants_filename, "r") as f:
-        targets_data = json.load(f)
-    
-    
-    
-    bboxes_gdf = gpd.read_file(targets_plants_filename) # Read in from file
-    bboxes_gdf['bounding_box'] = bboxes_gdf['bounding_box'].apply(loads) # str to Polygon
-    bboxes_gdf.set_geometry('bounding_box', inplace=True) # It is the primary geometry
-    bboxes_gdf = bboxes_gdf.drop(columns=['region_outline_version', 'geometry']) # remove confusing cols
-    bboxes_gdf = bboxes_gdf.set_crs(region_crs).to_crs(visualization_crs) # Needs CRS, then convert
-
-    
-    # bounding_boxes_gdf = gpd.GeoDataFrame(bboxes_gdf, geometry='geometry', crs=bboxes_gdf.crs)
-    # bounding_boxes_geojson = bounding_boxes_gdf.to_json()
 
     # Set up the map
     m = Map(center=(region_center.y, region_center.x),
@@ -728,56 +707,33 @@ def plot_targets_on_map(region_geojson, depots_filename, micro_routes_filename, 
         name=region_contour_data['name'])
     m.add(region_layer)
 
-    depot_points = GeoJSON(
-        data=depot_data,
-        style={'color': 'black', 'radius':10, 'fillColor': '#3366cc', 'opacity':0.5, 'weight':1.9, 'dashArray':'2', 'fillOpacity':0.6},
-        hover_style={'fillColor': 'red' , 'fillOpacity': 0.2},
-        point_style={'radius': 3, 'color': 'red', 'fillOpacity': 0.8, 'fillColor': 'blue', 'weight': 3},
-        draggable=True,
-        name=depot_data['name']
-    )
-    m.add(depot_points)
 
-    routes_layer = GeoJSON(
-        data=micro_routes_data, 
-        style={'color': 'green', 'fillColor': 'green', 'opacity': 0.75, 'weight': 4},
-        hover_style={'color': 'red' , 'opacity': 0.8, 'weight': 3},
-        name=f'Micro Routes'
-    )
-    # m.add(routes_layer)
+    targets_points_gdf = targets_gdf.copy()
+    targets_points_gdf = targets_points_gdf.drop(columns=['bounding_box']) # remove confusing cols
 
-    targets_layer = GeoJSON(
-        data=targets_data,
-        style={'color': 'black', 'radius':6, 'fillColor': 'red', 'opacity':0.5, 'weight':1, 'fillOpacity':0.6},
-        hover_style={'fillColor': 'red' , 'fillOpacity': 0.2},
-        point_style={'radius': 3, 'color': 'red', 'fillOpacity': 0.8, 'fillColor': 'blue', 'weight': 3},
-        draggable=True,
-        name=targets_data['name']
-    )
-    def on_click_target(event, feature, properties):
-        # print(event)
-        # print(feature)
-        # print(properties)
-        # print(len(targets_data['features']))
-        targets_data['features'] = [
-            feature for feature in targets_data['features']
-            if feature['properties']['target_id'] != properties['target_id']
-        ]
-        print(len(targets_data['features']))
-        targets_layer.data = targets_data
-
-    targets_layer.on_click(on_click_target)
-
-
+    targets_layer = GeoData(geo_dataframe = targets_points_gdf,
+                            style={'color': 'black', 'radius':6, 'fillColor': 'red', 'opacity':0.5, 'weight':1, 'fillOpacity':0.3},
+                            hover_style={'fillColor': 'red' , 'fillOpacity': 0.2},
+                            point_style={'radius': 3, 'color': 'red', 'fillOpacity': 0.8, 'fillColor': 'blue', 'weight': 3},
+                            draggable=True,
+                            name="Identified targets"
+                            )
     m.add(targets_layer)
+
+
+    bboxes_gdf = targets_gdf.copy()
+    # bboxes_gdf['bounding_box'] = bboxes_gdf['bounding_box'].apply(loads) # str to Polygon
+    bboxes_gdf.set_geometry('bounding_box', inplace=True) # It is the primary geometry
+    bboxes_gdf = bboxes_gdf.drop(columns=['geometry']) # remove confusing cols
+    # bboxes_gdf = bboxes_gdf.set_crs(region_crs).to_crs(visualization_crs) # Needs CRS, then convert
 
     bboxes_layer = GeoData(geo_dataframe = bboxes_gdf,
                    style={'color': 'red', 'opacity':0.5, 'weight':1.9,
-                          'fill': False, 'fillColor': 'red', 'fillOpacity': 0.2
-                          },
+                          'fill': False, 'fillColor': 'red', 'fillOpacity': 0.2 },
                    hover_style={'color': 'red' , 'opacity': 1.0, 'fill': False},
-                   name = 'Countries')
-    # m.add(bboxes_layer)
+                   name = 'Target bounding boxes')
+    m.add(bboxes_layer)
+
 
     draw_control = GeomanDrawControl()
     draw_control.circlemarker = {}
@@ -791,7 +747,7 @@ def plot_targets_on_map(region_geojson, depots_filename, micro_routes_filename, 
     m.add(ScaleControl(position='bottomleft'))
     return m
 
-m = plot_targets_on_map(region_contour_geojson, depots_filename, micro_routes_filename, targets_plants_filename)
+m = plot_targets_on_map(region_contour_geojson, targets_gdf)
 m
 
 # +
